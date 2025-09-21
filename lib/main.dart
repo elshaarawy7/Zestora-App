bd3705cf2295a89d312d24208a8f85ea1299c6d6
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-09-21 18:18:53 +0300

    change name app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ZestoraApp());
+}
+
+class ZestoraApp extends StatelessWidget {
+  const ZestoraApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: 
+    );
+  }
+}
