e5d1fec6139b53c6f219181ea37a1d383e7145c6
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-09-25 18:41:30 +0300

    add widget quintity select

--- lib/futcher/ui/app/prodatct_detiles.dart
+++ lib/futcher/ui/app/prodatct_detiles.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:zestora/core/utils/app_router.dart';
 import 'package:zestora/futcher/ui/model/prodact_model.dart';
+import 'package:zestora/futcher/ui/widgets/quintity_selector.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   final ProdactModel product;
@@ -9,30 +12,113 @@ class ProductDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            SizedBox(height: 30),
+
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    context.go(AppRouter.KHomePage);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back_ios,
+                    size: 30,
+                    color: Colors.black,
+                  ),
+                ),
+
+                SizedBox(width: 100),
+
+                Text(
+                  "Details",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                Spacer(),
+
+                Icon(Icons.favorite, color: Colors.red, size: 25),
+                SizedBox(width: 5),
+                Icon(Icons.share, size: 25, color: Colors.black),
+              ],
+            ),
+
+            SizedBox(height: 50),
             // صورة المنتج
             ClipRRect(
               borderRadius: BorderRadius.circular(16),
               child: Image.asset(
                 product.image,
-                height: 250,
+                height: 300,
                 width: double.infinity,
                 fit: BoxFit.cover,
               ),
             ),
             const SizedBox(height: 16),
 
-            // اسم المنتج
-            Text(
-              product.title,
-              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                3,
+                (index) => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: CircleAvatar(
+                    radius: 8,
+                    backgroundColor: index == 1 ? Colors.green : Colors.grey,
+                  ),
+                ),
+              ),
             ),
-            const SizedBox(height: 8),
+
+            SizedBox(height: 10),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  product.title,
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                QuantitySelector(
+                  initialValue: 1,
+                  onChanged: (value) {
+                    print(" $value");
+                  },
+                ),
+              ],
+            ), 
+
+
+            const SizedBox(height: 8), 
+
+            Text("A classic favorite! Indulge in a crispy, thin crust topped with rich tomato sauce, layers of gooey mozzarella cheese, and delicious pepperoni slices. Perfectly baked with a hint of herbs for a mouth-watering experience in every bite. Read more...", 
+            style: TextStyle(
+              fontSize: 16 , 
+              fontWeight: FontWeight.w400 , 
+              color: Colors.grey , 
+            ), 
+            overflow: TextOverflow.ellipsis, 
+            maxLines: 4,
+            ) ,   
+
+            SizedBox(height: 10,) , 
+
+            Text("Size" , style: TextStyle(
+              fontSize: 20 , 
+              fontWeight: FontWeight.bold , 
+              color: Colors.black ,
+            ),) , 
 
             // السعر
             Text(
@@ -42,8 +128,6 @@ class ProductDetailsScreen extends StatelessWidget {
             const SizedBox(height: 16),
 
             // وصف المنتج (ممكن تضيفه في الموديل)
-            
-
             const SizedBox(height: 24),
 
             // زر الإضافة للسلة
@@ -55,7 +139,7 @@ class ProductDetailsScreen extends StatelessWidget {
                 },
                 child: const Text("Add to Cart"),
               ),
-            )
+            ),
           ],
         ),
       ),
---
+++ lib/futcher/ui/widgets/quintity_selector.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class QuantitySelector extends StatefulWidget {
+  final int initialValue;
+  final ValueChanged<int>? onChanged;
+
+  const QuantitySelector({
+    super.key,
+    this.initialValue = 1,
+    this.onChanged,
+  });
+
+  @override
+  State<QuantitySelector> createState() => _QuantitySelectorState();
+}
+
+class _QuantitySelectorState extends State<QuantitySelector> {
+  late int quantity;
+
+  @override
+  void initState() {
+    super.initState();
+    quantity = widget.initialValue;
+  }
+
+  void _increment() {
+    setState(() {
+      quantity++;
+    });
+    widget.onChanged?.call(quantity);
+  }
+
+  void _decrement() {
+    if (quantity > 1) {
+      setState(() {
+        quantity--;
+      });
+      widget.onChanged?.call(quantity);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+
+        
+        // زرار ناقص
+        CircleAvatar(
+          backgroundColor: const Color(0xffF4F7F2),
+          radius: 20,
+          child: IconButton(
+            onPressed: _decrement,
+            icon: const Icon(Icons.remove, size: 20),
+          ),
+        ),
+
+        // العدد
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Text(
+            "$quantity",
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
+
+        // زرار زائد
+        CircleAvatar(
+          backgroundColor: const Color(0xffF4F7F2),
+          radius: 20,
+          child: IconButton(
+            onPressed: _increment,
+            icon: const Icon(Icons.add, size: 20),
+          ),
+        ),
+      ],
+    );
+  }
+}
