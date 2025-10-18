2ac7441f3691029de4141202a89751c9c3136a65
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-10-18 18:17:49 +0300

    create class cheakout repo

--- lib/core/utils/strip_servies.dart
+++ lib/core/utils/strip_servies.dart
@@ -1,3 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:zestora/core/utils/api_keys.dart';
+import 'package:zestora/core/utils/api_servies.dart';
+import 'package:zestora/futcher/data/model/payment_intet_input_model.dart';
+import 'package:zestora/futcher/data/model/payment_intet_model/payment_intet_model.dart';
+
 class StripServies {
-  
+   
+   final String secret_key = '' ; 
+   final ApiServies apiServies = ApiServies() ;
+   Future<PaymentIntetModel> createPaymentIntet( 
+    PaymentIntetInputModel paymentIntetInputModel
+   ) async {
+
+    var response = await apiServies.post(
+      body: paymentIntetInputModel.toJson(),
+       url: 'https://api.stripe.com/v1/payment_intents', 
+       token: ApiKeys.secret_key , 
+       ) ;  
+        var paymentIntentModel = PaymentIntetModel.fromJson(response.data);
+
+    return paymentIntentModel;
+
+   } 
+
+   Future initPaymentSheat({required String PaymentClientSycreat}) async {
+      await Stripe.instance.initPaymentSheet(
+      paymentSheetParameters: SetupPaymentSheetParameters(
+        paymentIntentClientSecret: PaymentClientSycreat , 
+        merchantDisplayName: "Elshaarawy"
+      )
+     ) ; 
+
+     Future disPlayPaymentSheet() async{
+      await Stripe.instance.presentCustomerSheet();
+     } 
+
+     Future mackpayment({required PaymentIntetInputModel paymentIntetInputModel}) async{
+      var paymentIntetModel = await createPaymentIntet(paymentIntetInputModel) ; 
+      await initPaymentSheat(
+        PaymentClientSycreat: paymentIntetModel.clientSecret!
+      ) ; 
+      await disPlayPaymentSheet() ;
+     }
+   }
 }
