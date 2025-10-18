8143001e636fb6271025a749bd6a9983ea3202ea
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-10-18 18:26:25 +0300

    create fuiler error

---
+++ lib/core/error/fuiler_eror.dart
@@ -0,0 +1,11 @@
+abstract class Fuiler {
+  final String errorMassge ;
+
+  Fuiler({required this.errorMassge});
+
+} 
+
+class SelverFuiler extends Fuiler{
+  SelverFuiler({required super.errorMassge});
+
+}
