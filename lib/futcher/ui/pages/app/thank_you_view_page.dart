43e845f853e2f62e89c27878e3502c6c11222f91
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-10-19 17:03:53 +0300

    create thank view page

---
+++ lib/futcher/ui/pages/app/thank_you_view_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ThankYouViewPage extends StatelessWidget {
+  const ThankYouViewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+    Scaffold();
+  }
+}
--- lib/futcher/ui/widgets/custem_batton_bloc_cunsumer.dart
+++ lib/futcher/ui/widgets/custem_batton_bloc_cunsumer.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:zestora/futcher/logic/manger/cubit/payemnt_cubit.dart';
+import 'package:zestora/futcher/ui/widgets/custem_batton.dart';
 
 class CustemBattonBlocCunsumer extends StatelessWidget {
   const CustemBattonBlocCunsumer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocConsumer<PayemntCubit , PayemntState>(
+      listener: (context, state) {
+        if(state is PayemntSucsess){
+          
+        }
+      },
+      builder: (context, state) {
+        return CustomGreenButton(
+          isLoding: state is PayemntCubit ? true : false,
+          text: 'contanue', onPressed: () {});
+      },
+    );
   }
 }
